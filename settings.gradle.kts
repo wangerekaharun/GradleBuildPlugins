355eeefc5d14d3ecba6cb64d1f6f385e2bb9b936
Harun Wangereka <harunwangereka@gmail.com>
2021-08-20 11:23:19 +0300

    Updated AS Version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,16 +7,16 @@ pluginManagement {
     }
 
     plugins {
-        id("com.android.application") version "4.2.2"
+        id("com.android.application") version "7.0.1"
         id("org.jetbrains.kotlin.android") version "1.5.20"
-        id("com.android.library") version "4.2.2"
+        id("com.android.library") version "7.0.1"
         id("com.google.firebase.crashlytics") version "2.1.0"
     }
 
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
-                "com.android.application", "com.android.library" -> useModule("com.android.tools.build:gradle:4.1.1")
+                "com.android.application", "com.android.library" -> useModule("com.android.tools.build:gradle:7.0.1")
                 "com.google.firebase.crashlytics" -> useModule("com.google.firebase:firebase-crashlytics-gradle:2.1.0")
             }
         }
