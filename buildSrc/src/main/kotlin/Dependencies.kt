ae369782b0710615d5f85528de74fef8027a9490
Harun Wangereka <harunwangereka@gmail.com>
2021-11-16 14:00:04 +0300

    Update Gradle version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -57,7 +57,7 @@ object TestLibraries {
 
 object AndroidSdk {
     const val minSdkVersion = 21
-    const val compileSdkVersion = 30
+    const val compileSdkVersion = 31
     const val targetSdkVersion = compileSdkVersion
     const val versionCode = 1
     const val versionName = "1.0"
